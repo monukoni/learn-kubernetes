ed6b3d6a1a5b7cf8ab914a40a9b5e52ffd78b0f8
monukoni <kartoplya213@gmail.com>
2026-02-11 00:26:47 +0200

    added state locking in s3

--- terraform/layers/app/backend.tf
+++ terraform/layers/app/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-project-state-bucket312"
-    key    = "terraform-app.tfstate"
-    region = "eu-central-1"
+    bucket       = "terraform-project-state-bucket312"
+    key          = "terraform-app.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
   }
 }
